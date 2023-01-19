7a79e32787d20c90e8a10b2f912dd5ff458f24c7
Nicola Corti <corti.nico@gmail.com>
2023-01-19 11:14:58 +0000

    Setup Grammarly playground

--- app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
+++ app/src/main/java/com/ncorti/kotlin/template/app/MainActivity.kt
@@ -2,9 +2,11 @@ package com.ncorti.kotlin.template.app
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.addTextChangedListener
 import com.ncorti.kotlin.template.app.databinding.ActivityMainBinding
 import com.ncorti.kotlin.template.library.FactorialCalculator
 import com.ncorti.kotlin.template.library.android.NotificationUtil
@@ -20,30 +22,39 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.buttonCompute.setOnClickListener {
-            if (binding.editTextFactorial.text.isNotEmpty()) {
-                val input = binding.editTextFactorial.text.toString().toLong()
-                val result = try {
-                    FactorialCalculator.computeFactorial(input).toString()
-                } catch (ex: IllegalStateException) {
-                    "Error: ${ex.message}"
-                }
+        binding.editTextFactorial.addTextChangedListener({ text: CharSequence?, start: Int, count: Int, after: Int ->
+//            Log.wtf("NCOR", "[before]: $text - start: $start - count: $count - after: $after")
+        }, { text: CharSequence?, start: Int, before: Int, count: Int ->
+            Log.wtf("NCOR", "[on]: ${text?.length} - start: $start - before: $before - count: $count")
+//            Log.wtf("NCOR", "[content]: $text")
+        }, { text ->
+//            Log.wtf("NCOR", "[after]: $text")
+        })
 
-                binding.textResult.text = result
-                binding.textResult.visibility = View.VISIBLE
-                notificationUtil.showNotification(
-                    context = this,
-                    title = getString(R.string.notification_title),
-                    message = result
-                )
-            } else {
-                Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
-            }
+        binding.buttonCompute.setOnClickListener {
+//            if (binding.editTextFactorial.text.isNotEmpty()) {
+//                val input = binding.editTextFactorial.text.toString().toLong()
+//                val result = try {
+//                    FactorialCalculator.computeFactorial(input).toString()
+//                } catch (ex: IllegalStateException) {
+//                    "Error: ${ex.message}"
+//                }
+//
+//                binding.textResult.text = result
+//                binding.textResult.visibility = View.VISIBLE
+//                notificationUtil.showNotification(
+//                    context = this,
+//                    title = getString(R.string.notification_title),
+//                    message = result
+//                )
+//            } else {
+//                Toast.makeText(this, "Please enter a number", Toast.LENGTH_SHORT).show()
+//            }
         }
 
         binding.buttonAppcompose.setOnClickListener {
-            val intent = Intent(it.context, ComposeActivity::class.java)
-            startActivity(intent)
+//            val intent = Intent(it.context, ComposeActivity::class.java)
+//            startActivity(intent)
         }
     }
 }
